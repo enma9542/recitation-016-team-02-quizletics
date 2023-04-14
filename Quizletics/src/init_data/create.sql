1a97dee2af38071a847e09a1128aaf0d0308b255
Madelyn Polly <mapo7968@colorado.edu>
2023-04-14 14:53:37 -0600

    create.sql created

---
+++ Quizletics/src/init_data/create.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS user_to_game CASCADE;
+DROP TABLE IF EXISTS users games;
+
+CREATE TABLE users(
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60) NOT NULL,
+);
+
+CREATE TABLE user_to_game(
+    username VARCHAR(50) PRIMARY KEY,
+    game_id UNIQUE SMALLINT NOT NULL,
+);
+
+CREATE TABLE games(
+    game_id SERIAL PRIMARY KEY,
+    score FLOAT,
+    total_time FLOAT,
+);
