b0a5d9b946a62bee8948cf688278ff2149be3f2a
enma9542 <enma9542@colorado.edu>
2023-04-24 13:52:49 -0600

    updated quiz.ejs, index.js, destroyed quiz.js. got a default quiz that has 5 questions working. random questions and answers are sorted randomly.

--- Quizletics/index.js
+++ Quizletics/index.js
@@ -65,10 +65,12 @@ app.use(
 
 //API Routes Go Here
 //quiz page
-app.get('/', (req, res) => {
+app.get('/quiz', (req, res) => {
   axios.get('https://the-trivia-api.com/v2/questions?limit=5&categories=history&difficulties=easy')
     .then(response => {
       const data = response.data;
+      res.render('pages/quiz', { data: data, score: req.session.score, req: req });
+      req.session.score = undefined;
       console.log('Data received from the API:', data);
       res.render('pages/quiz', { data: data });
     })
@@ -79,95 +81,98 @@ app.get('/', (req, res) => {
 });
 
 
+
+
+
 // app.get('/', (req, res) => {
 //   res.render('pages/home')
 // });
 
 //Test API
-app.get('/welcome', (req, res) => {
-  res.json({status: 'success', message: 'Welcome!'});
-});
-app.get('/', (req, res) => {
-    res.redirect('/login');
-});
+// app.get('/welcome', (req, res) => {
+//   res.json({status: 'success', message: 'Welcome!'});
+// });
+// app.get('/', (req, res) => {
+//     res.redirect('/login');
+// });
 
-app.get('/login', (req, res) => {
-    res.render('pages/login');
-});
+// app.get('/login', (req, res) => {
+//     res.render('pages/login');
+// });
 
-app.post('/login', async (req, res) => {
-    var username = req.body.username;
-    console.log(username);
-    var loginQuery = `SELECT * FROM users WHERE username = '${username}';`;
-    db.any(loginQuery)
-      .then(async data=>{
-        console.log("login:");
-        console.log(req.body.password);
-        console.log(data);
-        const match = await bcrypt.compare(req.body.password, data[0].password);
-        console.log(match);
-        if (match){
-            req.session.user = data;
-            req.session.save();
-            res.redirect('/home');
-        } else {
-          res.render('pages/login', {message: "Incorrect Password."});
-        }
-      })
-      .catch(err => {
-        console.log(`${err}`);
-        res.redirect('/register');
-      });
-});
+// app.post('/login', async (req, res) => {
+//     var username = req.body.username;
+//     console.log(username);
+//     var loginQuery = `SELECT * FROM users WHERE username = '${username}';`;
+//     db.any(loginQuery)
+//       .then(async data=>{
+//         console.log("login:");
+//         console.log(req.body.password);
+//         console.log(data);
+//         const match = await bcrypt.compare(req.body.password, data[0].password);
+//         console.log(match);
+//         if (match){
+//             req.session.user = data;
+//             req.session.save();
+//             res.redirect('/home');
+//         } else {
+//           res.render('pages/login', {message: "Incorrect Password."});
+//         }
+//       })
+//       .catch(err => {
+//         console.log(`${err}`);
+//         res.redirect('/register');
+//       });
+// });
 
-// Register
-app.post('/register', async (req, res) => {
-  //hash the password using bcrypt library
-  if(req.body.password != req.body.password_confirm){
-    res.render('pages/register', {message: "Passwords do not match", error: true});
-  }
-  const hash = await bcrypt.hash(req.body.password, 10);
+// // Register
+// app.post('/register', async (req, res) => {
+//   //hash the password using bcrypt library
+//   if(req.body.password != req.body.password_confirm){
+//     res.render('pages/register', {message: "Passwords do not match", error: true});
+//   }
+//   const hash = await bcrypt.hash(req.body.password, 10);
 
-  // To-DO: Insert username and hashed password into 'users' table
-  const searchQuery = "SELECT * FROM users where username = $1";
-  const insertQuery = "INSERT INTO users (username, password, email) VALUES ($1, $2, $3) returning *;"
-  const values = [req.body.username, hash, req.body.email];
+//   // To-DO: Insert username and hashed password into 'users' table
+//   const searchQuery = "SELECT * FROM users where username = $1";
+//   const insertQuery = "INSERT INTO users (username, password, email) VALUES ($1, $2, $3) returning *;"
+//   const values = [req.body.username, hash, req.body.email];
 
-  db.any(searchQuery, [req.body.username])
-  .then(data => {
-    if(data && (data.length > 0)){
-      res.render('pages/register', {message: "Username Already Exists, Please Choose New Username.", error: true});
-    }
-    else{
-      db.any(insertQuery, values)
-      .then(data => {
-        //console.log(data);
-        res.render('pages/login', {message: "User Added Successfully", error: false});
-      });
-    }
-  });
+//   db.any(searchQuery, [req.body.username])
+//   .then(data => {
+//     if(data && (data.length > 0)){
+//       res.render('pages/register', {message: "Username Already Exists, Please Choose New Username.", error: true});
+//     }
+//     else{
+//       db.any(insertQuery, values)
+//       .then(data => {
+//         //console.log(data);
+//         res.render('pages/login', {message: "User Added Successfully", error: false});
+//       });
+//     }
+//   });
 
-});
+// });
 
-app.get('/register', (req, res) => {
-  res.render("pages/register", {
-    message: msg,
-    error: msgerr,
-  });
-  msg = '';
-  msgerr = false;
-});
+// app.get('/register', (req, res) => {
+//   res.render("pages/register", {
+//     message: msg,
+//     error: msgerr,
+//   });
+//   msg = '';
+//   msgerr = false;
+// });
 
-app.get('/userProfile', (reg, res) =>{
-  var avgScore;
-  var highScore;
+// app.get('/userProfile', (reg, res) =>{
+//   var avgScore;
+//   var highScore;
 
-});
+// });
 
-app.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.render("pages/login", {message: 'Logged Out Successfully.'});
-});
+// app.get("/logout", (req, res) => {
+//   req.session.destroy();
+//   res.render("pages/login", {message: 'Logged Out Successfully.'});
+// });
 
 
 // Authentication Middleware.
