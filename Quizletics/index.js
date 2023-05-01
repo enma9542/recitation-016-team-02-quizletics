6d7e377be60c68dcdbef51c8a6e55f5b7b6084eb
enma9542 <enma9542@colorado.edu>
2023-05-01 13:29:29 -0600

    timer, score, changed quiz format, still no submit quiz post here yet though

--- Quizletics/index.js
+++ Quizletics/index.js
@@ -352,7 +352,7 @@ app.post("/submitQuiz", async (req, res) => {
   var valDiff = req.body.difficulty;
   var valCategory = req.body.category;
   var valUsername = req.session.user.username;
-  var valScore = (valNum_correct * 50) - (valTime * 10);
+  var valScore = Math.max((valNum_correct * 100) - (valTime * 5), 0);
   var gameVals = [valTime, valDiff, valCategory, valNum_correct, valScore];
 
   var insertGameQuery = `INSERT INTO games (time_taken, difficulty, category, num_correct, score) VALUES ($1, $2, $3, $4, $5) returning game_id;`;
