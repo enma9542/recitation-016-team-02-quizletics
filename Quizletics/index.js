6203e2494e5caf5c7d970af56c898b0cf7391a70
enma9542 <enma9542@colorado.edu>
2023-05-01 14:29:05 -0600

    message

--- Quizletics/index.js
+++ Quizletics/index.js
@@ -195,7 +195,6 @@ app.get('/userProfile', (req, res) =>{
     res.render("pages/login", {message: 'Must Be Logged In to View Profile Page', error: true});
   }
 
-  var user = req.session.user[0];
   var valUsername = req.session.user[0].username;
   var quizzesTaken;
   var pointsEarned;
@@ -344,7 +343,7 @@ app.post("/submitQuiz", async (req, res) => {
   var valDiff = req.body.difficulty;
   var valCategory = req.body.category;
   var valUsername = req.session.user.username;
-  var valScore = (valNum_correct * 100) - (valTime * 10);
+  var valScore = Math.max((valNum_correct * 100) - (valTime * 5), 0);
   var gameVals = [valTime, valDiff, valCategory, valNum_correct, valScore];
 
   var insertGameQuery = `INSERT INTO games (time_taken, difficulty, category, num_correct, score) VALUES ($1, $2, $3, $4, $5) returning game_id;`;
